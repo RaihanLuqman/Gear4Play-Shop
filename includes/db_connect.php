fa81156009ec82f1050d6c07291a9a652740c885
RaihanLuqman <121221105+RaihanLuqman@users.noreply.github.com>
2025-07-28 14:58:07 +0700

    Delete includes directory

--- includes/db_connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Connect to database
-$conn = new mysqli('localhost', 'root', '', 'gear4play_shop');
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-?>
